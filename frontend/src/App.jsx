64e65bbebfa166ab0a3ad1ea7f7b8f9e9abd4c1b
aleckshn <aleckshn@gmail.com>
2025-01-22 23:56:35 +1300

    created navigation bar with placeholder image for logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,9 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './index.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="" alt="Vite logo" />
-        </a>
-      </div>
-      <h1 className='text-9xl'>Vite + React</h1>
-      <div className="">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Header></Header>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <nav class="flex justify-end items-center text-[25px] p-5">
+            <img class="size-20 mr-auto" src="./assets/placeholder-logo.svg" alt="placeholder logo"></img>
+            <div class="flex gap-10 pr-10">
+                <h2>About us</h2>
+                <h2>Treatments</h2>
+                <h2>Review</h2>
+                <h2>Booking</h2>
+                <h2>Contact</h2>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
