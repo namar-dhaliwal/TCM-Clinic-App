420636ac3c40bc4210ac0af7e7b95ae78b50c3ca
Henry Gao <henrygao48@gmail.com>
2025-02-16 12:12:56 +1300

    Setup service layer for bookings service

---
+++ frontend/src/services/BookingService.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:4000/api/bookings'
+
+export const getBookings = async () => {
+    try {
+        const response = await axios.get(API_BASE_URL);
+        return response;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export const addBooking = async (booking) => {
+    try {
+        const response = await axios.post(API_BASE_URL, booking);
+        return response;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export const updateBooking = async (booking) => {
+    try {
+        const response = await axios.patch(`${API_BASE_URL}/${booking._id}`, booking);
+        return response;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+export const deleteBooking = async (bookingId) => {
+    try {
+        const response = await axios.delete(`${API_BASE_URL}/${bookingId}`);
+        return response;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
