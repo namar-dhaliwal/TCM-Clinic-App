f4cc1de652193ad93c7320e6597782d94dcbcacc
sli683 <sli683@aucklanduni.ac.nz>
2025-02-19 17:02:17 +1300

    image and text component finished

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 const Home = () => {
 
     return (
-      <div className="home">
-        <h2>Home</h2>
+      <div className="flex flex-wrap mb-[10rem] mx-auto justify-center">
+          <img class="size-20 w-[50rem] h-[40rem] object-cover mt-[10rem] mx-[5rem]" src="./assets/placeholder-logo.svg" alt="placeholder logo"></img>
+          <div className="w-[50rem] mx-[10rem] my-[10rem]">
+              <div className="text-[2rem] mb-[0.1rem]">Clinic/Practitioner</div>
+              <div className="text-[1.7rem] mb-[1.2rem] text-gray-500">SubHeading</div>
+              <div className="text-[1.4rem]">Body text, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br></br><br></br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
       </div>
     )
   }
