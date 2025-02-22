f77dc65300400b9376fd313afc04fcb532f4ba91
aleckshn <aleckshn@gmail.com>
2025-02-22 21:41:22 +1300

    Created about us section including placeholder image, title and lorem ipsum for body text

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -2,7 +2,18 @@ const AboutUs = () => {
 
     return (
         <div className="about">
-            <h2>About Us</h2>
+            <div class="flex flex-col justify-end items-center text-[25px] font-[300]">
+                <img class="size-[120px] mt-[50px]" src="./assets/placeholder-logo.svg" alt="placeholder logo"></img>
+                <h2 class="text-[75px] font-[650]">About Us (Clinic/practitioner)</h2>
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</h2>
+                <h2>sed do eiusmod tempor incididunt ut labore</h2>
+                <h2>et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</h2>
+                <h2>nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</h2>
+                <h2>fugiat nulla pariatur. Excepteur sint occaecat cupidatat</h2>
+                <h2>non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</h2>
+                <h2>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,</h2>
+                <h2 class="mb-[100px]">totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi </h2>
+            </div>
         </div>
     )
   }
