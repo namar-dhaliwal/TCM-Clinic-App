fe600e3670b559491970221d7b8994c454f8cef1
Henry Gao <henrygao48@gmail.com>
2025-02-23 13:09:03 +1300

    Update tailwind to hopefully resolve conflicts

--- frontend/src/components/admin/CreateBookingModal.jsx
+++ frontend/src/components/admin/CreateBookingModal.jsx
@@ -125,7 +125,7 @@ const CreateBookingModal = ({ isOpen, onClose, defaultStart, roomId }) => {
 				Add Booking
 			</h2>
 			{error && (
-				<div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4'>
+				<div className='bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded-sm mb-4'>
 					{error}
 				</div>
 			)}
