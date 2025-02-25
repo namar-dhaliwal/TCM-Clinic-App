1a06ab158e41a3fd26e358a6eb6f0c7ea7e64bdf
Jin <ajhz011@aucklanduni.ac.nz>
2025-02-26 12:53:34 +1300

    Setup vercel and added the frontend build into the backend. Also adjusted server.js so that it properly waits for mongodb to be connected.

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,47 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
 const webpageRoutes = require("./routes/webpages");
 const reviewRoutes = require("./routes/reviews");
 const bookingsRoutes = require("./routes/booking");
-const { errorHandler } = require("./middleware/error.js");
 
-//invoke express app
 const app = express();
 
-// more middlemare, vid 3 10:30
-app.use(express.json());
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONG_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error.message);
+    throw error; // If DB connection fails, the function will fail and return 500
+  }
+};
 
-// middleware to log routing requests that come in, from website itself or postman simulations
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
+module.exports = async (req, res) => {
+  try {
+    // Wait for MongoDB connection before proceeding with the request
+    await connectToMongoDB();
 
-// page routes
-app.use("/api/pages/", webpageRoutes);
-app.use("/api/pages/", reviewRoutes); // review routes, will change routing after
-app.use(`/api/bookings/`, bookingsRoutes);
+    // more middlemare, vid 3 10:30
+    app.use(express.json());
 
-// connect to db
-mongoose
-  .connect(process.env.MONG_URI)
-  .then(() => {
-    //listen for reqs
-    app.listen(process.env.PORT, () => {
-      console.log("connected to db & listening on port", process.env.PORT);
+    // middleware to log routing requests that come in, from website itself or postman simulations
+    app.use((req, res, next) => {
+      console.log(req.path, req.method);
+      next();
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+    // serving frontend staticcally
+    app.use(express.static('dist')) 
+
+    // page routes
+    app.use("/api/pages/", webpageRoutes);
+    app.use("/api/pages/", reviewRoutes); // review routes, will change routing after
+    app.use(`/api/bookings/`, bookingsRoutes);
+
+    // Return the app and handle request once everything has been set up properly
+    return app(req, res);
+  } catch (error) {
+    return res.status(500).send("Error connecting to the database or initializing routes.");
+  }
+};
